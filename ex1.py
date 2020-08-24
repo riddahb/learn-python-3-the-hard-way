89bd77eb492ccf24efc9ab6bd65b3dcc67963c62
Riddah Bashir <riddahb@gmail.com>
2020-08-25 00:14:56 +0100

    Add ex1.py ex2.py ex3.py

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# Zed Shaw, Learn Python 3 The Hard Way
+# Chapter 01
+# -------------------------------------
+
+# excersise 1
+print ('Hello World!')
+print ('Hello again!')
+print ('I like typing this!')
+print ('This is fun')
+print ('Yay! printing')
+print ("I'd rather much you 'not'touch this")

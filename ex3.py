9e0913b7bed47b7a03335c7fa4454cd63b667399
Riddah Bashir <riddahb@gmail.com>
2020-08-25 02:32:37 +0100

    update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,18 +1,28 @@
+# This will show the words "I will count my Chickens:"
 print ("I will now count my chickens:")
-
+#This will present the word "Hens" and calculate this calculation
 print("Hens", 25 + 30 / 6 )
+# This will print the word roosters and calculate the following calculation
 print("Roosters", 100 - 25 * 3 % 4)
-
+# This will present the words "Now I will count the eggs"
 print ("Now I will count the eggs:")
-
+# This will calculate the folloing calculation
 print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-
+# This will give the answer false to this question as 6 is larger than -2 however its saying 6 is smaller than -2 yielding the answer 'False'
 print ("Is it true that 3 + 2 < 5 - 7")
-
+# This will not print as there is no quotation marks
 print (3 + 2 < 5 - 7)
-
+# This will present the words of the question "what is 3 + 2" which will give the answer 5 from the calculation of 3 + 2 at the end = 5
 print ("What is 3 + 2?", 3 + 2)
+# This will present the question What is 5 - 7 which will give the answer to the calculation at the end of the question which is -2 (2nd part of the bracket i.e. the calculation gives an answer to that calculation)
 print ("What is 5 - 7?", 5 - 7)
-
+# Presents the words "Oh thats why it's false on the screen"
 print ("Oh that's why it's false")
-
+# Presents the words "How about some more" on the screen
+print ("How about some more.")
+# This presents the question "Is it greater ?" regarding the calculation (False or True) next to it so in this case it will give the answer True as 5 is bigger than -2
+print("Is it greater?", 5 > -2)
+# This presents this question and will give the answer as a true or false as for this answer it will present the words "TRUE"
+print("Is it greater or equal?", 5 >= -2)
+# This presents the question and will give the answer as a true or false as for this answer it will present the words "false"
+print("Is it less or equal?", 5 <= -2)
